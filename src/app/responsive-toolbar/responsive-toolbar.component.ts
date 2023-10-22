effbca7f2f68470a18ecb124ada9e4fa42d8d1b2
Dave Sharman <DaveSharman@shaw.ca>
2023-10-22 15:31:25 -0600

    upgrade to angular 10

--- src/app/responsive-toolbar/responsive-toolbar.component.ts
+++ src/app/responsive-toolbar/responsive-toolbar.component.ts
@@ -1,61 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+
 import { MenuItem } from '../menu-item';
 
 @Component({
   selector: 'app-responsive-toolbar',
   templateUrl: './responsive-toolbar.component.html',
-  styleUrls: ['./responsive-toolbar.component.scss']
+  styleUrls: ['./responsive-toolbar.component.scss'],
 })
 export class ResponsiveToolbarComponent implements OnInit {
-
   menuItems: MenuItem[] = [
     {
       label: 'Sign Up',
       icon: 'login',
       showOnMobile: true,
       showOnTablet: true,
-      showOnDesktop: true
+      showOnDesktop: true,
+      link: 'login',
     },
     {
       label: 'About',
       icon: 'help',
       showOnMobile: false,
       showOnTablet: true,
-      showOnDesktop: true
+      showOnDesktop: true,
+      link: 'about',
     },
     {
       label: 'Pricing',
       icon: 'attach_money',
       showOnMobile: false,
       showOnTablet: false,
-      showOnDesktop: true
+      showOnDesktop: true,
+      link: 'pricing',
     },
     {
       label: 'Docs',
       icon: 'notes',
       showOnMobile: false,
       showOnTablet: true,
-      showOnDesktop: true
+      showOnDesktop: true,
+      link: 'docs',
     },
     {
       label: 'Showcase',
       icon: 'slideshow',
       showOnMobile: false,
       showOnTablet: false,
-      showOnDesktop: true
+      showOnDesktop: true,
+      link: 'showcase',
     },
     {
       label: 'Blog',
       icon: 'rss_feed',
       showOnMobile: false,
       showOnTablet: false,
-      showOnDesktop: false
+      showOnDesktop: false,
+      link: 'blog',
     },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
